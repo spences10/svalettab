cf451e52e2cdebd9df456d1934fd90230c5e372b
spences10 <spences10apps@gmail.com>
2026-05-24 11:11:40 +0100

    chore: switch tooling to vite-plus

---
+++ .changeset/shy-moons-clap.md
@@ -0,0 +1,6 @@
+---
+'svalettab': patch
+---
+
+Switch project tooling from ESLint and Prettier to vite-plus check and
+vp exec workflows.
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,45 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { playwright } from '@vitest/browser-playwright';
-import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+export default {
 	plugins: [sveltekit()],
-
+	fmt: {
+		useTabs: true,
+		singleQuote: true,
+		printWidth: 70,
+		trailingComma: 'all',
+		proseWrap: 'always',
+	},
+	lint: {
+		ignorePatterns: [
+			'.svelte-kit/**',
+			'build/**',
+			'dist/**',
+			'dist-extension/**',
+		],
+		options: {
+			typeAware: false,
+			typeCheck: false,
+		},
+	},
 	test: {
 		expect: { requireAssertions: true },
-
 		projects: [
 			{
 				extends: './vite.config.ts',
-
 				test: {
 					name: 'client',
-
 					browser: {
 						enabled: true,
 						provider: playwright(),
 						instances: [{ browser: 'chromium', headless: true }],
 					},
-
 					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
 					exclude: ['src/lib/server/**'],
 				},
 			},
-
 			{
 				extends: './vite.config.ts',
-
 				test: {
 					name: 'server',
 					environment: 'node',
@@ -38,4 +49,4 @@ export default defineConfig({
 			},
 		],
 	},
-});
+};
